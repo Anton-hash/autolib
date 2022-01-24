fbdade8bb41af20180fb9ab79bbe1c31f6bbbf91
Anton Meshkel <mechkel@tut.by>
2022-01-24 15:24:56 +0300

    first commit

---
+++ src/main/java/com/meshkel/autolib/AutolibApplication.java
@@ -0,0 +1,13 @@
+package com.meshkel.autolib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutolibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutolibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meshkel/autolib/AutolibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meshkel.autolib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutolibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
